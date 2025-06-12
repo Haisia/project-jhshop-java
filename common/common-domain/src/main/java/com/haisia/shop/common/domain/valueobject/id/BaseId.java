082ae0f170704c5fd3a2ce71b763259c37bea9b8
최준혁 <cwnsgur13579@5finger.co.kr>
2025-06-12 11:03:55 +0900

    BaseId

--- common/common-domain/src/main/java/com/haisia/shop/common/domain/valueobject/id/BaseId.java
+++ common/common-domain/src/main/java/com/haisia/shop/common/domain/valueobject/id/BaseId.java
@@ -1,31 +1,6 @@
 package com.haisia.shop.common.domain.valueobject.id;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 public abstract class BaseId<T> implements Serializable {
-  private T value;
-
-  protected BaseId(T value) {
-    this.value = value;
-  }
-
-  public T getValue() {
-    return value;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (o == null || getClass() != o.getClass()) return false;
-    BaseId<?> baseId = (BaseId<?>) o;
-    return Objects.equals(value, baseId.value);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hashCode(value);
-  }
-
-  protected BaseId() {
-  }
 }
--- common/common-domain/src/main/java/com/haisia/shop/common/domain/valueobject/id/UserAuthId.java
+++ common/common-domain/src/main/java/com/haisia/shop/common/domain/valueobject/id/UserAuthId.java
@@ -2,15 +2,29 @@ package com.haisia.shop.common.domain.valueobject.id;
 
 import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.Objects;
 import java.util.UUID;
 
+@Getter
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Embeddable
 public class UserAuthId extends BaseId<UUID> {
   private UUID value;
-  public UserAuthId(UUID value) {
-    super(value);
+
+  @Override
+  public boolean equals(Object o) {
+    if (o == null || getClass() != o.getClass()) return false;
+    UserAuthId that = (UserAuthId) o;
+    return Objects.equals(value, that.value);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(value);
   }
 }
