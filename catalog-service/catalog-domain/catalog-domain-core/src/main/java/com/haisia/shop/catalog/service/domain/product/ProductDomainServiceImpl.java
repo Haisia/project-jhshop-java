db138423e7b10919c417ffa5ea0b8ac18fc36a92
최준혁 <cwnsgur13579@gmail.com>
2025-06-13 20:42:07 +0900

    build error

--- catalog-service/catalog-domain/catalog-domain-core/src/main/java/com/haisia/shop/catalog/service/domain/product/ProductDomainServiceImpl.java
+++ catalog-service/catalog-domain/catalog-domain-core/src/main/java/com/haisia/shop/catalog/service/domain/product/ProductDomainServiceImpl.java
@@ -3,11 +3,12 @@ package com.haisia.shop.catalog.service.domain.product;
 import com.haisia.shop.catalog.service.domain.product.entity.Product;
 import com.haisia.shop.catalog.service.domain.product.exception.ProductDomainException;
 import com.haisia.shop.catalog.service.domain.product.entity.ProductInitializer;
+import com.haisia.shop.common.domain.event.DomainEvent;
 
 public class ProductDomainServiceImpl implements ProductDomainService {
 
   @Override
-  public Object validateAndInitiate(Product product) {
+  public DomainEvent validateAndInitiate(Product product) {
     ProductInitializer productInitializer = new ProductInitializer(product, ProductDomainException::new);
     productInitializer.validateAndInitialize();
     return null;
--- catalog-service/catalog-domain/catalog-domain-core/src/main/java/com/haisia/shop/catalog/service/domain/seller/SellerDomainServiceImpl.java
+++ catalog-service/catalog-domain/catalog-domain-core/src/main/java/com/haisia/shop/catalog/service/domain/seller/SellerDomainServiceImpl.java
@@ -3,10 +3,11 @@ package com.haisia.shop.catalog.service.domain.seller;
 import com.haisia.shop.catalog.service.domain.seller.entity.Seller;
 import com.haisia.shop.catalog.service.domain.seller.entity.SellerInitializer;
 import com.haisia.shop.catalog.service.domain.seller.exception.SellerDomainException;
+import com.haisia.shop.common.domain.event.DomainEvent;
 
 public class SellerDomainServiceImpl implements SellerDomainService {
   @Override
-  public Object validateAndInitiate(Seller seller) {
+  public DomainEvent validateAndInitiate(Seller seller) {
     SellerInitializer sellerInitializer = new SellerInitializer(seller, SellerDomainException::new);
     sellerInitializer.validateAndInitialize();
     return null;
